2ec0a806023d3d38b670a71056a3705985d26764
Or Shemesh <orshemesh91@gmail.com>
2021-02-10 16:29:08 +0200

    bla

--- replace_repo_conf.sh
+++ replace_repo_conf.sh
@@ -3,5 +3,6 @@ git checkout -b tmp
 cp ~/.bashrc $PWD/.bashrc
 cp ~/.aliases $PWD/.aliases
 cp ~/.screenrc $PWD/.screenrc
-git commit -am "update repo conf files"
 git status
+git commit -am "update repo conf files"
+
