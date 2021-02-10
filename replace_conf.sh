9eec43051e2697edaa51d7a72a7a9a6b4f1d0577
orshemesh91 <orshemesh91@gmail.com>
2021-02-10 15:59:08 +0200

    Update replace_conf.sh

--- replace_conf.sh
+++ replace_conf.sh
@@ -1,15 +1,7 @@
 mv ~/.bashrc ~/.old_bashrc
 mv ~/.aliases ~/.old_aliases
 mv ~/.screenrc ~/.old_screenrc
-mv ~/.tmux.conf ~/.old_tmux.conf
-mv ~/.inputrc ~/.old_.inputrc
-mv ~/.globalrc ~/.old_.globalrc
 
 ln -s $PWD/.bashrc ~/.bashrc
 ln -s $PWD/.aliases ~/.aliases
 ln -s $PWD/.screenrc ~/.screenrc
-ln -s $PWD/.tmux.conf ~/.tmux.conf
-ln -s $PWD/.inputrc ~/.inputrc
-ln -s $PWD/.globalrc ~/.globalrc
-
-./replace_emacs_conf_only.sh
