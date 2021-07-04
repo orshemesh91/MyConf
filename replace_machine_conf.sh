af69a06b3c78a89c98aa505ad804c3c8e10f8d3a
Or Shemesh <orshemesh91@gmail.com>
2021-07-04 18:20:28 +0300

    tmp
    Signed-off-by: Or Shemesh <orshemesh91@gmail.com>

--- replace_machine_conf.sh
+++ replace_machine_conf.sh
@@ -1,14 +1,17 @@
 mv ~/.bashrc ~/.old_bashrc
+mv ~/.bash_profile ~/.old_bash_profile
 mv ~/.aliases ~/.old_aliases
 mv ~/.gdbinit ~/.old_gdbinit
 # mv ~/.screenrc ~/.old_screenrc
 
 ln -s $PWD/.bashrc ~/.bashrc
+ln -s $PWD/.bash_profile ~/.bash_profile
 ln -s $PWD/.aliases ~/.aliases
 ln -s $PWD/.gdbinit ~/.gdbinit
 # ln -s $PWD/.screenrc ~/.screenrc
 
 source $PWD/.bashrc ~/.bashrc
+source $PWD/.bash_profile ~/.bash_profile
 source $PWD/.aliases ~/.aliases
 # source $PWD/.screenrc ~/.screenrc
 
